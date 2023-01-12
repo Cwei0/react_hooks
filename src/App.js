7e0b230c7a1b2949e0c64fcde3fee7a81140cc78
Cwei0 <bottgabriel@icloud.com>
2023-01-12 22:00:55 +0100

    long!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { useReducer, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const reducer = (state, action) => {
+    switch(action.type) {
+        case "INCREMENT":
+            return {count: state.count + 1, showtext: state.showtext}
+        case "toggleShowText":
+            return {count: state.count, showtext: !state.showtext}
+        default:
+            return state;
+    }
+}
+const App = () => {
+    const[state, dispatch] = useReducer(reducer, {count: 0, showtext:true})
+    return ( 
+        <div>
+            <h2>{state.count}</h2>
+         <button onClick={() => {
+            dispatch({type: "INCREMENT" })
+            dispatch({type: "toggleShowText"})
+         }}>
+            Click me
+         </button>
+         {state.showtext && <h2>Bitch</h2>}
+        </div>
+         
+     );
 }
 
 export default App;
